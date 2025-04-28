0b64c6350e94ad6f721637cac68228f0acf3716e
sreenath1212 <ssreenathhpd@gmail.com>
2025-04-28 11:35:43 +0530

    Update fastbot.py

--- fastbot.py
+++ fastbot.py
@@ -16,7 +16,7 @@ st.set_page_config(
     page_title="🎓 College Info Assistant",
     page_icon="🤖",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapsed"
 )
 
 # Inject the viewport meta tag
