7d24d4cc08010405abf63a2243886e23b202c1b5
sreenath1212 <ssreenathhpd@gmail.com>
2025-05-01 19:57:38 +0530

    Update fastbot.py

--- fastbot.py
+++ fastbot.py
@@ -64,7 +64,7 @@ OPENROUTER_API_KEYS = [
     st.secrets["OPENROUTER_API_KEY_4"],
     # Add more keys as needed
 ]
-MODEL ='meta-llama/llama-4-maverick:free'
+MODEL ='google/gemini-pro-1.5'
 #'mistralai/mistral-small-3.1-24b-instruct:free'
 #'deepseek/deepseek-v3-base:free'
 #'meta-llama/llama-4-scout:free'
